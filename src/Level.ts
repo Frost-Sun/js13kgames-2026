cd0444bbc731560ce808b161bc593e65f0de1fe3
Sami Heikkinen <521design@gmail.com>
2026-09-12 12:12:08 +0300

    fix: finishing not adding again as finished

--- src/Level.ts
+++ src/Level.ts
@@ -279,7 +279,10 @@ export const updateLevel = (
         if (o.type === "character") {
             moveObject(time, level, o);
 
-            if (includesArea(level.finishArea, o)) {
+            if (
+                o.action !== GameObjectAction.Finish &&
+                includesArea(level.finishArea, o)
+            ) {
                 level.charactersFinished++;
                 playTune(SFX_HOME);
 
